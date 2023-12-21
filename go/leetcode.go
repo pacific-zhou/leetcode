a4b8f5ed783af65d5059d6ac88b64ecb0c83cfc5
pacific-zhou <printfzhou@outlook.com>
2023-12-21 23:02:43 +0800

    feat:  add 2 --story=1

--- go/leetcode.go
+++ go/leetcode.go
@@ -5,6 +5,46 @@ import (
 	"math"
 )
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func leetcode2(l1 *ListNode, l2 *ListNode) *ListNode {
+	var head, tail *ListNode
+	carry := 0
+	for l1 != nil || l2 != nil {
+		n1, n2 := 0, 0
+		if l1 != nil {
+			n1 = l1.Val
+		}
+		if l2 != nil {
+			n2 = l2.Val
+		}
+		sum := n1 + n2 + carry
+		if head == nil {
+			head = &ListNode{Val: sum % 10}
+			tail = head
+		} else {
+			tail.Next = &ListNode{Val: sum % 10}
+			tail = tail.Next
+		}
+		carry = sum / 10
+		if l1 != nil {
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			l2 = l2.Next
+		}
+	}
+	if carry > 0 {
+		tail.Next = &ListNode{
+			Val: carry,
+		}
+	}
+	return head
+}
+
 func leetcode26(nums []int) int {
 	slow, fast := 0, 1
 	for fast < len(nums) {
