0eea1bcbf160039592bcaa03d1afac957bc21b89
pacific-zhou <pacific-zhou@outlook.com>
2025-02-13 23:47:39 +0800

    add leetcode_94

--- cpp/leetcode.cpp
+++ cpp/leetcode.cpp
@@ -1343,6 +1343,25 @@ public:
 };
 #endif
 
+#ifdef leetcode_94
+class Solution {
+public:
+    void traverse(TreeNode* root, vector<int>& ans) {
+        if (!root) {
+            return;
+        }
+        traverse(root->left, ans);
+        ans.push_back(root->val);
+        traverse(root->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        traverse(root, ans);
+        return ans;
+    }
+};
+#endif
+
 // main entry point
 int main() {
   cout << "Hello, World!" << endl;
